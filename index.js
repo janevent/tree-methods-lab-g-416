a5762f3758b534d74aa026d9ac3b0fb299a45b7b
Jane Venter <janeanneventer@gmail.com>
2020-08-27 04:54:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -34,11 +34,7 @@ function findOrAdd(rootNode, newNode){
   }
 }
 
-// let node = {data: 5, left:
-//                       {data: 3, left: null, right: null},
-//                         right: {data: 7, left: null,
-//                         right: {data: 9, left: null, right: null}
-//                       }}
+
 function max(rootNode){
   let currentNode = rootNode;
   if(rootNode.right){
